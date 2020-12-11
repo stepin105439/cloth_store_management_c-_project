a3fd60cd504d810b72039127a407ccb0f83ca3bd
stepin105439 <69083026+stepin105439@users.noreply.github.com>
2020-12-11 20:32:08 +0530

    Add files via upload

---
+++ cloth_store_management.h
@@ -0,0 +1,18 @@
+#ifndef __CLOTH_STORE_H__
+	#define __CLOTH_STORE_H__
+
+	#include<fstream>
+    #include<conio.h>
+    #include<string.h>
+    #include<stdio.h>
+    #include<iostream>
+    using namespace std;
+
+
+
+    void get();
+    
+    int add_item();
+    int search_item();
+    int cut_item();
+    #endif
