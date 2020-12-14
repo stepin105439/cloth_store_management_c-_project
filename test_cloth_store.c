ef4e97b77959a50eb8ac7ce09f03e8cd8b25d959
stepin105439 <69083026+stepin105439@users.noreply.github.com>
2020-12-14 11:20:24 +0530

    Add files via upload

---
+++ test_cloth_store.c
@@ -0,0 +1,16 @@
+#include"cloth_store_management.h"
+#include <gtest/gtest.h>
+namespace {
+TEST(Cloth_Store_Management_Test,Add_Item) {
+EXPECT_EQ("coat",add_item("coat"));
+}
+
+TEST(Cloth_Store_Management_Test,Search_Item) {
+EXPECT_EQ("coat",search_item("coat"));
+}
+
+TEST(Cloth_Store_Management_Test,Cut_Item) {
+EXPECT_EQ("jeans","cut_item("jeans"));
+}
+}
+
